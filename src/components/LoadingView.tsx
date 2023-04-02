7da58913d4e4b27d12d068092623807ca3c6ec17
Dipankar Biswas <dipankarbd@gmail.com>
2023-04-02 09:05:45 +0600

    add loading view

---
+++ src/components/LoadingView.tsx
@@ -0,0 +1,11 @@
+import './LoadingView.css';
+
+const LoadingView = () => {
+  return (
+    <div className="LoadingView">
+      <h3>Loading...</h3>;
+    </div>
+  );
+};
+
+export default LoadingView;
